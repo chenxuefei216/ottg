5948aa5d5241d47c37092ae38ae8433663dc7d20
Demo User <demouser@n236-121.mtholyoke.edu>
2016-01-05 11:21:19 -0500

    Expanded the implementation of Edith/s functional test.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,22 +13,34 @@ class NewVisitorTest(unittest.TestCase):
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
-
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage.
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         # She is invited to enter a to-do itme straight away
+        inputbox = self.broswer.find_element_by_id('id_new_item')
+        assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feather" into a text box
         # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1. Buy peacock feathers" as an item in a to-do lists
+        inputbox.send_keys(Key.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make fly"
@@ -43,5 +56,5 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
         self.fail('Finish the app!')
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
