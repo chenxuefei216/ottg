44a629d2e6924952cb0405c25ad6ed5462fcb36e
Demo User <demouser@n236-121.mtholyoke.edu>
2016-01-07 11:46:30 -0500

    Associated lists with items.

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -3,5 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # extending models.Model giving all the saving and retriving methods we need in test.py
+class List(models.Model):
+    pass
+    
 class Item(models.Model):
-    text = models.TextField(default = "")
+    text = models.TextField(default = '')
+    list = models.ForeignKey(List, default = None)
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -43,8 +43,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        new_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=new_list)
+        Item.objects.create(text='itemey 2', list=new_list)
 
         response=self.client.get('/lists/the-only-list-in-the-world/')
 
