0c98ba90e0635f46658d118c1c6c129712b021b5
Demo User <demouser@n236-31.mtholyoke.edu>
2016-01-12 10:55:07 -0500

    Separated model and view tests.

--- superlists/lists/tests/test_view.py
+++ superlists/lists/tests/test_view.py
@@ -92,32 +92,3 @@ class ListViewTest(TestCase):
         # response.context['list'] tells us
         # what list did the response passed in when it renders
         self.assertEqual(response.context['list'], correct_list)
-
-class ItemAndListModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items_in_list(self):
-        new_list= List()
-        new_list.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = new_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = new_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(new_list, saved_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, new_list)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, new_list)
